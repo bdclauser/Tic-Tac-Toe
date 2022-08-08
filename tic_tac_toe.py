4d31631e37b519e402398cc06c7c507c3f5fb660
bdclauser <pzljug@hotmail.com>
2022-08-07 20:29:39 -0400

    fixed type in game logic

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -29,8 +29,15 @@ class TicTacToeGame:
         self.winner_combo = []
         self._current_moves = []
         self._has_winner = False
-        self._winning_combox = []
+        self._winning_combos = []
         self._setup_board()
+
+    def _setup_board(self):
+        self._current_moves = [
+            [Move(row, col) for col in range(self.board_size)]
+            for row in range(self.board_size)
+        ]
+        self._winning_combos = self._get_winning_combos()
 
 # lets define the board
 class TicTacToeBoard(tk.Tk):
