cca8a33d4cacb45833c5f3e30472b1e789bcfe54
bdclauser <pzljug@hotmail.com>
2022-08-09 20:44:18 -0400

    added tied condition

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -75,6 +75,14 @@ class TicTacToeGame:
         #Return true if the game has a winner
         return self._has_winner
 
+    def is_tied(self):
+        #Return true if the game is tied
+        no_winner = not self._has_winner
+        played_moves = (
+            move.label for row in self._current_moves for move in row
+        )
+        return no_winner and all(played_moves)
+
 # lets define the board
 class TicTacToeBoard(tk.Tk):
     def __init__(self):
